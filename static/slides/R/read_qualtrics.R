7a64c4106428d72dd45d70c52c4a37e0620a8e9d
Michael W. Kearney <kearneymw@missouri.edu>
2018-08-28 21:46:26 -0500

    update homework

--- static/slides/R/read_qualtrics.R
+++ static/slides/R/read_qualtrics.R
@@ -8,7 +8,13 @@ is_csv <- function(x) {
 }
 
 
-
+#' read qualtrics CSV
+#' 
+#' Reads and formats data exported from qualtrics.
+#' 
+#' @param Path to CSV exported from Qualtrics
+#' @return A tibble
+#' @export
 read_qualtrics <- function(x) {
   ## check to make sure it's a valid csv file
   is_csv(x)
@@ -28,26 +34,39 @@ read_qualtrics <- function(x) {
   names(x) <- tolower(names(x))
   
   ## convert to tibble
-  x <- tibble::as_tibble(x, validate = FALSE)
+  x <- tibble::as_tibble(x)
   
   ## add text (other box entries) columns
   attr(x, "descriptions") <- vars
   x
 }
 
+is_likert5 <- function(x) {
+  if (is.factor(x)) {
+    x <- as.character(x)
+  }
+  if (!is.character(x)) return(FALSE)
+  o <- unique(x)
+  if (length(o) > 6) return(FALSE)
+  if (all(is.na(o))) return(FALSE)
+  l <- c("^agree\\s?strong\\S{0,}$|^strong\\S{0,}\\s?agree$", 
+    "^disagree\\s?strong\\S{0,}$|^strong\\S{0,}\\s?disagree$", 
+    "^agree$", "^disagree$", "^neither")
+  all(vapply(l, function(x) any(!grepl(x, o, ignore.case = TRUE)), logical(1), USE.NAMES = FALSE))
+}
 
 recode_likert <- function(x) {
-  if (!any(grepl("^agree$", x, ignore.case = TRUE))) return(x)
-  ifelse(grepl("strongly agree|^agree strongly", x, ignore.case = TRUE), 5, 
+  if (!is_likert5(x)) return(x)
+  ifelse(grepl("^agree\\s?strong\\S{0,}$|^strong\\S{0,}\\s?agree$", x, ignore.case = TRUE), 5, 
     ifelse(grepl("^agree$", x, ignore.case = TRUE), 4,
-      ifelse(grepl("neither", x, ignore.case = TRUE), 3, 
+      ifelse(grepl("^neither", x, ignore.case = TRUE), 3, 
         ifelse(grepl("^disagree$", x, ignore.case = TRUE), 2,
-          ifelse(grepl("strongly disagree|^disagree strongly", x, ignore.case = TRUE), 1, NA_real_)))))
+          ifelse(grepl("^disagree\\s?strong\\S{0,}$|^strong\\S{0,}\\s?disagree$", 
+                       x, ignore.case = TRUE), 1, NA_real_)))))
 }
 
 recode_likerts <- function(x) {
-  chr <- vapply(x, is.character, logical(1))
-  x[chr] <- lapply(x[chr], recode_likert)
+  x[1:ncol(x)] <- lapply(x, recode_likert)
   x
 }
 
