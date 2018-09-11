bd87560d515e25aa656cdb4a2f61229e8e9e8f0f
Michael W. Kearney <kearneymw@missouri.edu>
2018-09-11 15:25:55 -0500

    add build trick

---
+++ static/build.R
@@ -0,0 +1,3 @@
+download.file("https://github.com/mkearney/stat/archive/master.zip", "~/Downloads/stat-course")
+unzip("~/Downloads/stat-course", exdir = "~/Desktop")
+browseURL("~/Desktop/stat-master/stat.Rproj")
