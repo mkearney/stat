e83253c3b743785e7f34f297af8c87b9913130a8
Michael W. Kearney <kearneymw@missouri.edu>
2018-09-18 15:20:30 -0500

    day 5 commit

---
+++ R/build-course.R
@@ -0,0 +1,3 @@
+download.file("https://github.com/mkearney/stat/archive/master.zip", "~/journ801stat-course")
+unzip("~/journ801stat-course", exdir = "~")
+browseURL("~/journ801stat-course/stat.rproj")
